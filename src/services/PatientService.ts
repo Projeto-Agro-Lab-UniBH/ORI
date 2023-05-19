9a7b4e7de77503bc5797617880ca3d04cd98cae9
Marlon <Marlonnlmx@outlook.com>
2023-05-19 14:51:55 -0300

    Formated with prettier

--- src/services/PatientService.ts
+++ src/services/PatientService.ts
@@ -1,25 +1,87 @@
 import { type } from "os";
 import { Patient } from "../interfaces/Patient";
-import { api } from "../providers/Api"
+import { api } from "../providers/Api";
 
-type patientProfileResponse = Patient
-type listAllPatientsResponse = Pick<Patient, "id" | "profile_photo" | "name" | "specie" | "gender" | "type" | "weight" | "situation" | "diagnosis" | "physical_shape">
-type editPatientDataRequest = Partial<Pick<Patient, "profile_photo" | "name" | "owner" | "specie" | "gender" | "type" | "weight" | "situation" | "diagnosis" | "physical_shape" | "entry_date" | "departure_date">>
+type patientProfileResponse = Patient;
+type listAllPatientsResponse = Pick<
+  Patient,
+  | "id"
+  | "profile_photo"
+  | "name"
+  | "specie"
+  | "gender"
+  | "type"
+  | "weight"
+  | "situation"
+  | "diagnosis"
+  | "physical_shape"
+>;
+type editPatientDataRequest = Partial<
+  Pick<
+    Patient,
+    | "profile_photo"
+    | "name"
+    | "owner"
+    | "specie"
+    | "gender"
+    | "type"
+    | "weight"
+    | "situation"
+    | "diagnosis"
+    | "physical_shape"
+    | "entry_date"
+    | "departure_date"
+  >
+>;
 
-function editPatientData(id: any, { profile_photo, name, owner, specie, gender, type, weight, situation, diagnosis, physical_shape, entry_date, departure_date }: editPatientDataRequest) {
-  return api.patch<patientProfileResponse>(`/animal/${id}`, { profile_photo, name, owner, specie, gender, type, weight, situation, diagnosis, physical_shape, entry_date, departure_date })
+function editPatientData(
+  id: any,
+  {
+    profile_photo,
+    name,
+    owner,
+    specie,
+    gender,
+    type,
+    weight,
+    situation,
+    diagnosis,
+    physical_shape,
+    entry_date,
+    departure_date,
+  }: editPatientDataRequest
+) {
+  return api.patch<patientProfileResponse>(`/animal/${id}`, {
+    profile_photo,
+    name,
+    owner,
+    specie,
+    gender,
+    type,
+    weight,
+    situation,
+    diagnosis,
+    physical_shape,
+    entry_date,
+    departure_date,
+  });
 }
 
 function getPatientProfile(id: string) {
-  return api.get<patientProfileResponse>(`/animal/${id}`)
+  return api.get<patientProfileResponse>(`/animal/${id}`);
 }
 
 function filterByAnimalType(type: string) {
-  return api.get<listAllPatientsResponse[]>(`/animal/filter/${type}`)
+  return api.get<listAllPatientsResponse[]>(`/animal/filter/${type}`);
 }
 
 function listAllPatients() {
-  return api.get<listAllPatientsResponse[] | null>('/animal/')
+  return api.get<listAllPatientsResponse[] | null>("/animal/");
 }
 
-export const PatientService = { patientProfile: getPatientProfile, listAllPatients, filterByAnimalType, editPatientData }
+export const PatientService = {
+  patientProfile: getPatientProfile,
+  listAllPatients,
+  filterByAnimalType,
+  editPatientData,
+};
