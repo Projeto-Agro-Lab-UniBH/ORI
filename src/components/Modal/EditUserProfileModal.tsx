9b4e45bcfcf13a5d65be807f333d0380656ca0e3
Marlon <Marlonnlmx@outlook.com>
2023-06-10 14:27:12 -0300

    fix: adding z-index to fix a component error

--- src/components/Modal/EditUserProfileModal.tsx
+++ src/components/Modal/EditUserProfileModal.tsx
@@ -108,8 +108,8 @@ const EditUserProfileModal = (props: IEditUserProfileModal) => {
         </Dialog.Trigger>
       </div>
       <Dialog.Portal>
-        <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-        <Dialog.Content className="w-[720px] rounded-lg border border-gray-200 fixed pt-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white overflow-hidden">
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed z-10" />
+        <Dialog.Content className="w-[720px] rounded-lg border border-gray-200 bg-white fixed overflow-hidden pt-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
           <div className="w-full px-6 pb-4 border-b-[1px] border-gray-200 flex items-center flex-row justify-between">
             <Dialog.Title className="font-semibold text-2xl">
               Editar dados do seu perfil
