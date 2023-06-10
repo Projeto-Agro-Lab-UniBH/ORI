057640a3dab046b2752bebb3f5502f404aa71b5b
Marlon <Marlonnlmx@outlook.com>
2023-06-10 01:42:54 -0300

    fix: import name corrected

--- src/providers/getAPIClient.ts
+++ src/providers/getAPIClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parseCookies } from "nookies";
 
 export function getAPIClient(ctx?: any) {
-  const { "nextauth.token": token } = parseCookies();
+  const { "nextauth.token": token } = parseCookies(ctx);
 
   const api = axios.create({ baseURL: "http://localhost:5000" });
 
