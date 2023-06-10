d07e747196fd87191fe35238d6ce34a44bdbccda
Marlon <Marlonnlmx@outlook.com>
2023-06-10 02:09:41 -0300

    add: ReactQueryDevtools component implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import '../styles/load.css';
-import 'tailwindcss/tailwind.css';
-import { QueryClientProvider } from 'react-query';
-import { queryClient } from '../providers/QueryClient';
-import { AuthProvider } from '../contexts/AuthContext';
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import "../styles/load.css";
+import "tailwindcss/tailwind.css";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClientProvider } from "react-query"
+import { queryClient } from "../providers/QueryClient";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-	return (
+  return (
 		<AuthProvider>
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
+				{/* <ReactQueryDevtools initialIsOpen={false} /> */}
 			</QueryClientProvider>
 		</AuthProvider>
-	);
+  );
 }
 
 export default MyApp;
