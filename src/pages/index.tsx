8bbbd8b8418d4ccccb0e1afa3f0e1fb095a9fc43
Marlon <Marlonnlmx@outlook.com>
2023-06-10 01:30:47 -0300

    feat: components code refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,15 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
 import Header from "../components/Header/Header";
-import { Option } from "../interfaces/Option";
-import { api } from "../providers/Api";
-import { useAuthContext } from "../contexts/AuthContext";
+import Feed from "../components/Feed/Feed";
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
-import Feed from "../components/Feed/Feed";
-
-type ListAllPatientsResponse = {
-  id: string;
-  profile_photo?: string;
-  name: string;
-  specie: string;
-  race: string;
-  gender: string;
-  weight: string;
-  prognosis: string;
-  diagnosis: Option[];
-  physical_shape: string;
-};
+import { useAuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
   const { user } = useAuthContext();
-  const [searchInput, setSearchInput] = useState<string>("");
-
-  const { data, isLoading } = useQuery<ListAllPatientsResponse[] | null>({
-    queryKey: ["pacient-list"],
-    queryFn: async () => {
-      const response = await api.get("/patient");
-      return response.data;
-    },
-  });
-
-  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(event.target.value);
-  };
 
   return (
-    <div className="w-full h-full flex flex-col justify-center px-14 mt-4 mb-20 gap-6">
-      <Header currentUser={user} />
+    <div className="w-full h-full flex flex-col justify-center px-14 mt-4 mb-10 gap-6">
+      <Header user={user} />
       <Feed />
     </div>
   );
@@ -47,7 +17,7 @@ export default function Home() {
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { ["nextauth.token"]: token } = parseCookies(ctx);
-
+  
   if (!token) {
     return {
       redirect: {
