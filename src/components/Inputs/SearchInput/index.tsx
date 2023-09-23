0c901498de69c05cf82f440475c143f332b47765
Marlon <Marlonnlmx@outlook.com>
2023-09-23 01:29:16 -0300

    feat: novo input para busca implementado

--- src/components/Inputs/SearchInput/index.tsx
+++ src/components/Inputs/SearchInput/index.tsx
@@ -1,187 +1,117 @@
-import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
-import { useState, useRef, useEffect, Dispatch, SetStateAction } from "react";
-import { useRouter } from "next/router";
+import * as Dialog from "@radix-ui/react-dialog";
 import DotsLoad from "../../Load/DotsLoad";
-import useSearch from "../../../hooks/useSearch";
+import VerticalScrollbarContainer from "../../Scrollbar/VerticalScrollbar";
 import SearchPatientResultItem from "../../Items/SearchPatientResultItem";
+import { api } from "../../../providers/Api";
+import { useQuery } from "react-query";
+import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Dispatch, SetStateAction, useState } from "react";
+import { SearchPatientResponse } from "../../../@types/ApiResponse";
 
-const SearchInput = ({
-  value,
+const SearchInput = ({ 
+  value, 
   setValue,
-  onChange,
-  onClick,
-}: {
-  value: string;
+  onChange 
+}:{
+  value: string; 
   setValue: Dispatch<SetStateAction<string>>;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onClick: () => void;
 }) => {
-  const router = useRouter();
-  const [isListOpen, setIsListOpen] = useState(false);
+  const [open, setOpen] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const listRef = useRef<HTMLDivElement | null>(null);
 
-  const { data } = useSearch({ router, searchInputValue: value, setIsLoading });
-
-  // Fecha a lista quando o usuário clica fora dela
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        listRef.current &&
-        inputRef.current &&
-        !listRef.current.contains(event.target as Node) &&
-        !inputRef.current.contains(event.target as Node)
-      ) {
-        setIsListOpen(false);
-      }
+  const { data } = useQuery<SearchPatientResponse[]>(
+    ["searchByInputValue", value],
+    async () => {
+      setIsLoading(true);
+      const response = await api.get<SearchPatientResponse[]>(
+        `/patient/search/by/values?search=${value || ""}`
+      );
+      setIsLoading(false);
+      return response.data;
+    },
+    {
+      enabled: value !== "", // Ativa a busca assim que algo for digitado
+      refetchOnReconnect: false,
     }
-
-    window.addEventListener("click", handleClickOutside);
-
-    return () => {
-      window.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
-
-  // Abre a lista quando o usuário clica no input
-  const handleInputClick = () => {
-    setIsListOpen(true);
-  };
-
-  const clearInput = async () => {
-    setValue("");
-
-    if (router.query.search != "") {
-      const query = { ...router.query, ["search"]: "", page: "1" };
-
-      await router.push({
-        pathname: router.pathname,
-        query,
-      });
-    }
-  };
+  )
 
   return (
-    <div className="w-[592px] flex">
-      <div className="w-[592px] flex flex-col">
-        <div className="relative w-full">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <MagnifyingGlassIcon color="#808080" width={16} height={16} />
-          </div>
-          <input
-            ref={inputRef}
-            type="text"
-            className="w-full h-10 pl-10 p-2.5 block bg-white border border-gray-200 rounded font-normal text-shark-950 text-sm hover:border-[#b3b3b3]"
-            placeholder="Procure um paciente"
-            value={value}
-            onChange={onChange}
-            onClick={handleInputClick}
-          />
-          {value !== "" && (
-            <button
-              type="button"
-              onClick={clearInput}
-              className="absolute inset-y-0 right-0 flex items-center pr-3"
-            >
-              <Cross2Icon
-                width={16}
-                height={16}
-                className="text-gray-500 hover:text-gray-600"
-              />
-            </button>
-          )}
-        </div>
-        {isListOpen && (value !== "" || isLoading) && (
-          <div className="w-[592px]">
-            <div
-              ref={listRef}
-              className="w-[592px] drop-shadow-lg absolute mt-2 z-10"
-            >
-              <div className="w-[592px] flex flex-col bg-white border border-gray-200 rounded">
-                {isLoading ? (
-                  <div className="w-full h-12 px-4 py-4 flex justify-center items-center">
-                    <DotsLoad />
-                  </div>
-                ) : (
-                  <>
-                    <ul className="w-full list-none">
+    <Dialog.Root onOpenChange={setOpen} open={open}>
+      <Dialog.Trigger className="w-[592px] h-10 border border-slate-300 rounded px-3 p-3 gap-2 flex items-center hover:hover:border-slate-400 hover:bg-slate-200/20 focus:outline-none">
+        <MagnifyingGlassIcon color="#94a3b8" width={16} height={16} />
+        <span className="text-sm text-slate-400">Pesquisar um paciente</span>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed z-10" />
+        <Dialog.Content className="w-[592px] rounded border-none fixed overflow-hidden top-12 left-1/2 -translate-x-1/2 z-10">
+          <div className="w-full flex">
+            <div className="w-full flex flex-col">
+              <div className="w-full relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <MagnifyingGlassIcon color="#94a3b8" width={16} height={16} />
+                </div>
+                <input
+                  type="text"
+                  className="w-full h-10 pl-10 p-3 block bg-white border-b border-slate-200 rounded-t font-normal text-shark-950 text-sm placeholder:text-slate-400 focus:outline-none"
+                  placeholder="Pesquisar um paciente"
+                  value={value}
+                  onChange={onChange}
+                />
+                {value !== "" && (
+                  <button
+                    type="button"
+                    onClick={() => setValue("")}
+                    className="absolute inset-y-0 right-0 flex items-center pr-3"
+                  >
+                    <Cross2Icon
+                      width={16}
+                      height={16}
+                      className="text-slate-500 hover:text-slate-600"
+                    />
+                  </button>
+                )}
+              </div>
+              <VerticalScrollbarContainer styleViewportArea="w-full bg-white max-h-[488px] overflow-y-scroll">
+                {(value !== "" || isLoading) &&
+                  (isLoading ? (
+                    <div className="w-full h-12 px-4 py-4 flex justify-center items-center">
+                      <DotsLoad />
+                    </div>
+                  ) : (
+                    <ul
+                      role="list"
+                      className="w-full divide-y divide-slate-200"
+                    >
                       {data && data.length > 0 ? (
-                        data && data.length >= 3 ? (
-                          data.slice(0, 3).map((data) => (
-                            <li
-                              key={data.id}
-                              className="w-full flex items-center border-b px-4 py-4"
-                            >
-                              <SearchPatientResultItem
-                                key={data.id}
-                                id={data.id}
-                                src={data.profile_photo}
-                                name={data.name}
-                                specie={data.specie}
-                                race={data.race}
-                              />
-                            </li>
-                          ))
-                        ) : (
-                          data.map((data, i) => {
-                            if (i == 1) {
-                              return (
-                                <li
-                                  key={data.id}
-                                  className="w-full flex items-center px-4 py-4"
-                                >
-                                  <SearchPatientResultItem
-                                    key={data.id}
-                                    id={data.id}
-                                    src={data.profile_photo}
-                                    name={data.name}
-                                    specie={data.specie}
-                                    race={data.race}
-                                  />
-                                </li>
-                              );
-                            }
-                            return (
-                              <li
-                                key={data.id}
-                                className="w-full flex items-center border-b px-4 py-4"
-                              >
-                                <SearchPatientResultItem
-                                  key={data.id}
-                                  id={data.id}
-                                  src={data.profile_photo}
-                                  name={data.name}
-                                  specie={data.specie}
-                                  race={data.race}
-                                />
-                              </li>
-                            );
-                          })
-                        )
+                        data.map((data) => (
+                          <li
+                            key={data.id}
+                            className="w-full flex items-center px-4 py-4"
+                          >
+                            <SearchPatientResultItem
+                              id={data.id}
+                              name={data.name}
+                              src={data.profile_photo}
+                              specie={data.specie}
+                              race={data.race}
+                            />
+                          </li>
+                        ))
                       ) : (
-                        <li className="w-full font-normal text-shark-950 text-base px-4 py-4">
+                        <li className="w-full font-normal text-slate-700 text-base px-4 py-4">
                           Nenhum resultado encontrado
                         </li>
                       )}
                     </ul>
-                    {data && data.length >= 3 && (
-                      <button
-                        onClick={onClick}
-                        className="w-full h-10 font-normal text-sm"
-                      >
-                        Ver todos os resultados
-                      </button>
-                    )}
-                  </>
-                )}
-              </div>
+                  ))}
+              </VerticalScrollbarContainer>
             </div>
           </div>
-        )}
-      </div>
-    </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
-export default SearchInput;
+export default SearchInput; 
